e413f959ac772700a4dd69106df47d6b87640bbd
Marcus Rohrmoser <work@mro.name>
2021-08-15 23:04:52 +0200

    initial boundary declaration mandatory.

--- test/parse_test.ml
+++ test/parse_test.ml
@@ -1,9 +1,9 @@
 let test_parse0 () =
   let fn = "dumps/2020-12-02T110016.post" in
   let st = Unix.stat fn in
-  Assert2.equals_int "uhu" 235484 st.st_size;
+  Assert2.equals_int "uhu" 235570 st.st_size;
   let ic = open_in_gen [ Open_rdonly; Open_binary ] 0o222 fn in
-  match Lib.Rfc2388.process ic "/tmp/" with
+  match Lib.Rfc2388.process ic "/dev/null" with
   (* | Ok { Part.name = n; filename = None; mime = None } ->
      Assert2.equals_string "uhu" "title" n
   *)
